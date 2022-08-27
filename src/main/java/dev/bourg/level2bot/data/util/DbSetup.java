140ad70d1a0711bb4abbfb073149aed3993522fa
B0urg <dj.ka@outlook.de>
2022-08-27 17:21:36 +0200

    Added class to execute dbsetup.sql

---
+++ src/main/java/dev/bourg/level2bot/data/util/DbSetup.java
@@ -0,0 +1,45 @@
+package dev.bourg.level2bot.data.util;
+
+import dev.bourg.level2bot.Level2Bot;
+import org.slf4j.Logger;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class DbSetup {
+
+    private static final Logger log = getLogger(DbSetup.class);
+
+    public static void initDb(DataSource dataSource) throws IOException, SQLException {
+
+        String setup;
+        try(InputStream in = DbSetup.class.getClassLoader().getResourceAsStream("./sql/dbsetup.sql")){
+            setup = new String(in.readAllBytes());
+        }catch (IOException e){
+            log.error("Could not read db setup file", e);
+            throw e;
+        }
+
+        String[] queries = setup.split(";");
+        try (Connection conn = dataSource.getConnection()){
+            conn.setAutoCommit(false);
+
+            for(String query : queries){
+
+                if(query.isBlank() || query.startsWith("//")) continue;
+                try (PreparedStatement stmt = conn.prepareStatement(query)){
+                    stmt.execute();
+                }
+            }
+            conn.commit();
+        }
+        log.info("Database setup completed.");
+    }
+
+}
